b1add60d46a96bd800d70a148755b0df36c7302b
sepiariver <info@sepiariver.com>
2016-08-17 23:01:56 -0700

    support templates in tv

--- core/components/ampify/elements/plugins/ampify.plugin.php
+++ core/components/ampify/elements/plugins/ampify.plugin.php
@@ -8,32 +8,53 @@
  * 
  **/
 
+// AMPIFY Context
 $amp_context = $modx->getOption('amp_context', $scriptProperties, 'amp', true);
+$countCtx = $modx->getCount('modContext', array('key' => $amp_context));
+if ($countCtx !== 1) {
+    $modx->log(modX::LOG_LEVEL_ERROR, 'AMPIFY could not find a Context with key: ' . $amp_context);
+    return;
+}
+
+// AMPIFY Template
+$amp_template = $modx->getOption('amp_template', $scriptProperties, '');
+$countTpl = $modx->getCount('modTemplate', $amp_template);
+if ($countTpl !== 1) {
+    $modx->log(modX::LOG_LEVEL_ERROR, 'AMPIFY requires a valid, default Template ID');
+    return;
+}
+
+// AMPIFY TV
+$amp_tv = $modx->getOption('amp_tv', $scriptProperties, '');
+$tvCol = (is_int($amp_tv)) ? 'id' : 'name';
+$countTv = $modx->getCount('modTemplateVar', array($tvCol => $amp_tv));
 
 $event = $modx->event->name;
 switch ($event) {
     
     case 'OnLoadWebDocument':
 
-        // Check Context TODO: make configurable
-        if ($modx->context->get('key') !== $amp_context) return;
+        // Only fire on AMP Context
+        if ($modx->context->get('key') !== $amp_context) {
+            break;
+        }
 
         // Check Resource
-        if (!($modx->resource instanceof modResource)) return;
+        if (!($modx->resource instanceof modResource)) {
+            break;
+        }
         
-        // Check Template
-        $amp_template = $modx->getOption('amp_template', $scriptProperties, 6);
-        $count = $modx->getCount('modTemplate', $amp_template);
-        if ($count !== 1) {
-            $modx->log(modX::LOG_LEVEL_ERROR, 'AMPIFY was not provided a valid Template ID');
-            return;
+        // Check Resource AMP TV
+        if ($countTv === 1) {
+            $tvValue = $modx->resource->getTVValue($amp_tv);
+            $countTvTpl = $modx->getCount('modTemplate', $tvValue);
+            if ($countTvTpl === 1) $amp_template = $tvValue;
         }
         
         // Set runtime resource property
         $modx->resource->set('template', $amp_template);
         
         // Move on
-        return true;
         break;
         
     case 'OnDocFormSave':
@@ -44,16 +65,23 @@ switch ($event) {
         // Check Resource
         if (!($resource instanceof modResource)) return;
         
+        // Check Resource AMP TV
+        if ($countTv === 1) {
+            $tvValue = $resource->getTVValue($amp_tv);
+            if (!$tvValue) break;
+        }
+        
+        // Set criteria for ContextResource object
         $criteria = array(
             'context_key' => $amp_context,
             'resource' => $resource->get('id'),
         );
         
-        // Check Resource Context
-        $rc = $modx->getObject('modContextResource', $criteria);
+        // Check ContextResource
+        $countCtxRes = $modx->getCount('modContextResource', $criteria);
         
         // Create if it doesn't exist
-        if (!$rc) {
+        if ($countCtxRes < 1) {
             
             $rc = $modx->newObject('modContextResource');
             // Use set(). It's not an xPDOSimpleObject            
@@ -66,13 +94,15 @@ switch ($event) {
             // Trigger the Context Gateway
             $modx->cacheManager->refresh();
         }
-
-        return true;
-        break;
         
+        // We're done
+        break;
+    
+    // Don't do anything on other events
     default:
         break;
         
 }
 
+// The Plugin returns nothing
 return;
